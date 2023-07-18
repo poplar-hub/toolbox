dfcf4bc92370afe3d8da14b1c1a6b5d1fb52d01a
poplar-hub <redgeeting+oschina@gmail.com>
2023-07-18 09:40:27 +0800

    注解生成

---
+++ src/test/java/com/dabbler/tools/ast/java/AnonationElementTest.java
@@ -0,0 +1,17 @@
+package com.dabbler.tools.ast.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author poplar-hub
+ * @version 1.0
+ * @date 2023/7/12
+ */
+class AnonationElementTest {
+
+    @Test
+    void getFormattedContent() {
+    }
+}
